370c44883b116c1258be7a36b6725a243e9e9825
Paul Adams <ghusiyy@gmail.com>
2024-02-16 23:48:50 -0800

    use state in example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import AppCalendarInput from "./components/AppCalendarInput";
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState("");
+
   return (
     <>
-      <AppCalendarInput label="Select date:" />
+      <AppCalendarInput
+        label="Select date:"
+        value={selectedDate}
+        onChange={(e) => setSelectedDate(e.target.value)}
+      />
 
       <p className="read-the-docs">
         Click <a>here</a> to learn more
