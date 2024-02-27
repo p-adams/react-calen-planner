59c3638038699f15c431efc7702a0fe12606aff5
Paul Adams <ghusiyy@gmail.com>
2024-02-27 00:05:32 -0800

    update Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import "./Modal.css";
-function Modal() {
-  return <dialog className="modal"></dialog>;
+interface ModalProps {
+  isOpen: boolean;
+  close?: () => {};
+}
+function Modal(props: ModalProps) {
+  const [internalIsOpen, setInternalIsOpen] = useState(false);
+
+  // Use external isOpen if provided, otherwise use internal state
+  const isOpen = props.isOpen !== undefined ? props.isOpen : internalIsOpen;
+
+  const close = () => {
+    // Call external close if provided, otherwise update internal state
+    if (props.close) {
+      props.close();
+    } else {
+      setInternalIsOpen(false);
+    }
+  };
+
+  return <dialog className="modal" open={isOpen} onClose={close}></dialog>;
 }
 
 export default Modal;
